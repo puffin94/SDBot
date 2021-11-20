a0bb25ab813393c3228783b769207f956374d4b4
Jack Lundy <jlundy06@qub.ac.uk>
2021-11-20 10:54:38 +0000

    Initial setup for the project

---
+++ src/test/java/com/lundy/jack/SDBot/SdBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lundy.jack.SDBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
